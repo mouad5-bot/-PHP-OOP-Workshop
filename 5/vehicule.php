f6f9eb456fc35ebafa6a8adbf93300b929bb9599
MOUAD FIFEL <mouad.fifel5@gmail.com>
2023-01-12 22:53:29 +0100

    start five question

---
+++ 5/vehicule.php
@@ -0,0 +1,10 @@
+<?php
+class Vehicule
+{
+    public $make ;
+    public $model ;
+    public $color ;
+    protected $noOfWheels ;
+    private $engineNumber ;
+    public static $counter = 0 ;
+}
